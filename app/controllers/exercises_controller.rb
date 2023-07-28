432da94af7de33586f2b4efb02cec814060eb57e
keisuzuki <kei.nishimura0131@gmail.com>
2023-07-28 14:36:40 +0900

    active_record_task2

--- app/controllers/exercises_controller.rb
+++ app/controllers/exercises_controller.rb
@@ -2,13 +2,13 @@ class ExercisesController < ApplicationController
   def exercise1
     # 【要件】注文されていないすべての料理を返すこと
     #   * left_outer_joinsを使うこと
-    @foods = Food.left_outer_joins(:order_foods).distinct
+    @foods = Food.left_outer_joins(foods: :order_foods).distinct.where(foods: { order_foods: { id: nil } })
   end
 
   def exercise2
     # 【要件】注文されていない料理を提供しているすべてのお店を返すこと
     #   * left_outer_joinsを使うこと
-    @shops = Shop
+    @shops = Shop.left_outer_joins()
   end
 
   def exercise3 
